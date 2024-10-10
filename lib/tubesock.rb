d184ea7acb2e1d2f98ab1d6ce74877bacbd2233a
Sebastian Serth <Sebastian.Serth@hpi.de>
2024-10-11 00:10:48 +0200

    Catch more errors when sending and receiving data
    Includes all changes from joshleblanc/tubesock

--- lib/tubesock.rb
+++ lib/tubesock.rb
@@ -49,7 +49,7 @@ class Tubesock
       type: type
     )
     @socket.write frame.to_s
-  rescue IOError, Errno::EPIPE, Errno::ETIMEDOUT
+  rescue IOError, Errno::EPIPE, Errno::ETIMEDOUT, Errno::ECONNRESET
     close
   end
 
@@ -168,7 +168,7 @@ class Tubesock
         end
       end
     end
-  rescue Errno::EHOSTUNREACH, Errno::ETIMEDOUT, Errno::ECONNRESET, IOError, Errno::EBADF
+  rescue Errno::EHOSTUNREACH, Errno::ETIMEDOUT, Errno::ECONNRESET, IOError, Errno::EBADF, Errno::ENOTSOCK, Errno::ECONNABORTED
     nil # client disconnected or timed out
   end
 end
